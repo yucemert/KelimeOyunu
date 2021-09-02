80279c22ce0a68e25c043918494b4f3126d3909e
Mert Yüce <yucemert97@gmail.com>
2021-09-02 12:19:17 +0300

    KelimeOyunu

---
+++ buton.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buton : MonoBehaviour
+{
+    manager manage;
+    Image renk;
+    RectTransform buyukluk;
+    string harf;
+    bool harf_verildi=false;
+    public bool yok_ol=false;
+    float kuculme_miktari=0.08f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manage=GameObject.Find("manager").GetComponent<manager>();
+        renk=GetComponent<Image>();
+        buyukluk=GetComponent<RectTransform>();
+        harf=gameObject.name;
+        
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        if(manage.tiklandi==false)
+        {
+            harf_verildi=false;
+            renk.color=Color.white;
+
+        }
+        if(yok_ol==true)
+        {
+            buyukluk.localScale-=new Vector3(kuculme_miktari,kuculme_miktari,kuculme_miktari);
+            if(buyukluk.localScale.x<=0)
+            Destroy(gameObject);
+
+
+        }
+
+    }
+
+
+    public void yesil_ol()
+    {
+        if(manage.tiklandi==true)
+        {
+            renk.color=Color.green;
+            if(harf_verildi==false)
+            {
+                manage.isaretli_buton_olustur(gameObject);
+                harf_verildi=true;
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
---
+++ manager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour
+{
+    public string[] sozluk;
+    public Text score;
+    List<GameObject> isaretli_butonlar;
+    string kelime=null;
+    public bool tiklandi=false;
+    public GameObject bitti_panel;
+    int puan=0;
+    int bulunan_kelime_sayisi=0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isaretli_butonlar=new List<GameObject>();
+    }
+
+    public void isaretli_buton_olustur(GameObject buton)
+    {
+        isaretli_butonlar.Add(buton);
+        kelime=null;
+        foreach(GameObject butonlar in isaretli_butonlar)
+        {
+            kelime=kelime+butonlar.name;
+            score.text=kelime;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        tiklandi=true;
+        if(Input.GetMouseButtonUp(0))
+        {
+            tiklandi=false;
+            yazi_olustur();
+            score.text=puan.ToString();
+
+        }
+
+
+    }
+
+
+void yazi_olustur()
+{
+    foreach(string kelimeler in sozluk)
+    {
+        if(kelimeler==kelime)
+        {
+            puan+=100;
+            bulunan_kelime_sayisi++;
+            foreach(GameObject buton in isaretli_butonlar)
+            {
+                buton.GetComponent<buton>().yok_ol=true;
+            }
+
+
+        }
+
+
+
+
+    }
+
+    isaretli_butonlar.Clear();
+    kelime=null;
+    if(bulunan_kelime_sayisi==sozluk.Length)
+    bitti_panel.SetActive(true);
+    
+
+
+
+
+
+}
+
+
+
+
+
+
+}
